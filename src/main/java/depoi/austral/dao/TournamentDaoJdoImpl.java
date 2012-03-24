b6998cf6d525359399540a3cabf768406577a4d7
amiura <agustin.miura@gmail.com>
2012-03-24 17:56:23 -0300

    Fixed the license.

--- src/main/java/depoi/austral/dao/TournamentDaoJdoImpl.java
+++ src/main/java/depoi/austral/dao/TournamentDaoJdoImpl.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 Miura Agustín
- * 					  Rozanect Jose	 
+ * 					  Rozanec Jose	 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
